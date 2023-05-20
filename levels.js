a84e3b19ebbe9caafe940740ab8db1b2ef67fbd2
vladisluw <komkov222111@gmail.com>
2023-05-20 13:52:29 +0500

    Подготовка к git

--- levels.js
+++ levels.js
@@ -1,35 +1,38 @@
 
 
  const levels ={
+  random(){return Math.round(Math.random() * (Object.keys(levels).length - 1) + 1)},
   1(){
-    console.log("Вы зашли в комнату. Перед вами странный сундук и проход дальше. Открыть или идти дальше?")
-    const action = readlineSync.question("Сундук  /  Вперед\n")
+    let random = Math.round(Math.random() * (Object.keys(levels).length - 1) + 1);
+    console.log("Вы зашли в комнату. Перед вами странный сундук и проход дальше. Открыть или идти дальше?\n")
+    const action = readlineSync.question("Открыть сундук  /  Идти дальше\n").trim().toLowerCase();
   
-    if(action === 'Сундук'){console.log('Сундук оказался притаившимся мимиком. Одной жертвой этого замка больше...\n Вы умерли!'); 
+    if(action === '1'){console.log('Сундук оказался притаившимся мимиком. Одной жертвой этого замка больше...\n Вы умерли!\n'); 
     process.exit()}
+    
+    else if (action === '2') {console.log('Вы пошли дальше по коридору'); 
+    console.log(this[this.random()]())}
 
-    else if (action === 'Вперед') {console.log('Вы пошли дальше по коридору'); 
-    console.log(this[Math.round(Math.random() * (2 - 1) + 1)]())}
-
-    else{console.log('Неверный ввод\n\n\n\n'); console.log(this['1']())}
+    else{console.log('Неверный ввод\n'); console.log(this['1']())}
   },
 
   2(){
-    console.log("Вы зашли в комнату. Перед вами ржавая лесница наверх и явно ненадежная лесница в подвал")
-    const action = readlineSync.question("1.Наверх   2.В подвал").trim().toLowerCase();
+    let random = Math.round(Math.random() * (Object.keys(levels).length - 1) + 1);
+    console.log("Вы зашли в комнату. Перед вами ржавая лесница наверх и явно ненадежная лесница в подвал\n")
+    const action = readlineSync.question("Наверх   В подвал\n").trim().toLowerCase();
   
-    if(action === '1'){console.log('Вы пошли дальше!'); 
-    process.exit()}
+    if(action === '1'){console.log('Вы пошли дальше!\n'); 
+    console.log(this[this.random()]())}
 
-    else if (action === '2') {console.log('Лесница вниз действительно оказалась ненадежной и сломалась под вашим весом. Вы падаете на пол.'); 
+    else if (action === '2') {console.log('Лесница вниз действительно оказалась ненадежной и сломалась под вашим весом. Вы падаете на пол.\n'); 
     status.hp -= 50
-    console.log(`-50 HP. HP: ${status.hp}/100`)
-    if (status.hp < 1) {{console.log('Вы умерли!'); 
+    console.log(`-50 HP. HP: ${status.hp}/100\n`)
+    if (status.hp < 1) {{console.log('Вы умерли!\n'); 
     process.exit()}}
-    console.log(this[Math.round(Math.random() * (2 - 1) + 1)]())
+    console.log(this[this.random()]())
     }
   
-    else{console.log('Неверный ввод\n\n\n\n');console.log(this['2']())}
+    else{console.log('Неверный ввод\n');console.log(this['2']())}
     },
 }
 
--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 /**Global */
 var readlineSync = require('readline-sync');
 var levels = require("./levels.js");
-
+let random = Math.round(Math.random() * (2 - 1) + 1)
 
 
 /**Main */
-console.log("Приветствую хуе мое потом продумаю")
-readlineSync.question('Чтобы начать игру нажмите Enter');
+console.log("Приветствую хуе мое потом продумаю\n")
+readlineSync.question('Чтобы начать игру нажмите Enter\n');
 
- 
-//console.log(levels[toString(Math.round(Math.random() * (2 - 1) + 1))]())
-console.log(typeof (toString(Math.round(Math.random() * (2 - 1) + 1))))
-console.log((toString(Math.round(Math.random() * (2 - 1) + 1))))
+
+console.log(levels[random]())
+
