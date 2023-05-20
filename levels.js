0fb966940628a9db2054acf2a6d86ab9ef3a1b04
vladisluw <komkov222111@gmail.com>
2023-05-20 14:23:17 +0500

    Подготовка к git

--- levels.js
+++ levels.js
@@ -1,48 +1,45 @@
-
-
- const levels ={
-  random(){return Math.round(Math.random() * (Object.keys(levels).length - 1) + 1)},
-  1(){
-    let random = Math.round(Math.random() * (Object.keys(levels).length - 1) + 1);
-    console.log("Вы зашли в комнату. Перед вами странный сундук и проход дальше. Открыть или идти дальше?\n")
-    const action = readlineSync.question("Открыть сундук  /  Идти дальше\n").trim().toLowerCase();
-  
-    if(action === '1'){console.log('Сундук оказался притаившимся мимиком. Одной жертвой этого замка больше...\n Вы умерли!\n'); 
-    process.exit()}
-    
-    else if (action === '2') {console.log('Вы пошли дальше по коридору'); 
-    console.log(this[this.random()]())}
-
-    else{console.log('Неверный ввод\n'); console.log(this['1']())}
-  },
-
-  2(){
-    let random = Math.round(Math.random() * (Object.keys(levels).length - 1) + 1);
-    console.log("Вы зашли в комнату. Перед вами ржавая лесница наверх и явно ненадежная лесница в подвал\n")
-    const action = readlineSync.question("Наверх   В подвал\n").trim().toLowerCase();
-  
-    if(action === '1'){console.log('Вы пошли дальше!\n'); 
-    console.log(this[this.random()]())}
-
-    else if (action === '2') {console.log('Лесница вниз действительно оказалась ненадежной и сломалась под вашим весом. Вы падаете на пол.\n'); 
-    status.hp -= 50
-    console.log(`-50 HP. HP: ${status.hp}/100\n`)
-    if (status.hp < 1) {{console.log('Вы умерли!\n'); 
-    process.exit()}}
-    console.log(this[this.random()]())
-    }
-  
-    else{console.log('Неверный ввод\n');console.log(this['2']())}
-    },
-}
-
-
+/**Экспорты */
 var readlineSync = require('readline-sync');
-module.exports = levels;
-
-
+var name = require("./main.js");
 var Inventory = require("./inventory.js");
 var Body = require("./character.js");
 
-var inv = new Inventory;
-var status = new Body;
+/**Создание нового героя */
+const inv = new Inventory;
+const status = new Body(name);
+
+const levels ={
+  
+  1(){
+    console.log(`\nВы зашли в комнату. Перед вами странный сундук и проход дальше. Открыть или идти дальше?\n`)
+    const action = readlineSync.question("\nОткрыть сундук  /  Идти дальше\n").trim().toLowerCase();
+  
+    if(action === '1'){console.log('\nСундук оказался притаившимся мимиком. Одной жертвой этого замка больше...\n Вы умерли!\n'); 
+    process.exit()}
+    
+    else if (action === '2') {console.log('\nВы пошли дальше по коридору\n'); 
+    console.log(this[Math.round(Math.random() * (Object.keys(levels).length - 1) + 1)]())}
+
+    else{console.log('\nНеверный ввод\n'); console.log(this['1']())}
+  },
+
+  2(){
+    console.log("\nВы зашли в комнату. Перед вами ржавая лесница наверх и явно ненадежная лесница в подвал\n")
+    const action = readlineSync.question("Наверх   В подвал\n").trim().toLowerCase();
+  
+    if(action === '1'){console.log('Вы пошли дальше!\n'); 
+    console.log(this[Math.round(Math.random() * (Object.keys(levels).length - 1) + 1)]())}
+
+    else if (action === '2') {console.log('\nЛесница вниз действительно оказалась ненадежной и сломалась под вашим весом. Вы падаете на пол.\n'); 
+    status.hp -= 50
+    console.log(`-50 HP. HP: ${status.hp}/100\n`)
+    if (status.hp < 1) {{console.log('\nВы умерли!\n'); 
+    process.exit()}}
+    console.log(this[Math.round(Math.random() * (Object.keys(levels).length - 1) + 1)]())
+    }
+  
+    else{console.log('\nНеверный ввод\n');console.log(this['2']())}
+    },
+}
+
+module.exports = levels;
--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-/**Global */
+/** Exports */
+
 var readlineSync = require('readline-sync');
 var levels = require("./levels.js");
-let random = Math.round(Math.random() * (2 - 1) + 1)
-
 
 /**Main */
 console.log("Приветствую хуе мое потом продумаю\n")
-readlineSync.question('Чтобы начать игру нажмите Enter\n');
+const name = readlineSync.question('\nЧтобы начать игру введите имя и нажмите Enter\n');
+module.exports = name
+
+console.log(levels[Math.round(Math.random() * (Object.keys(levels).length - 1) + 1)]())
 
 
-console.log(levels[random]())
 
